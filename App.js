cebaeeb804270f2976211e7aebada2908b29ca9c
Arham Jain <arham.nec@gmail.com>
2021-11-22 18:15:43 +0530

    feat(mutation): removed mutation blocker plugin

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const PaginationFilterPlugin = require("./plugins/PaginationFilter");
 const ConnectionFilterPlugin = require("postgraphile-plugin-connection-filter");
-const MutationBlockerPlugin = require("./plugins/MutationBlocker")
 const {postgraphile} = require("postgraphile");
-const config = require('./volume/config/config.json');
+const config = require("./config/config.json");
 
 const app = express();
 
 const services = config.services;
 
 for (const [service, connectionString] of Object.entries(services)) {
+    console.log(service, connectionString)
     app.use(
         postgraphile(connectionString, "public", {
+            disableDefaultMutations: true,
             watchPg: true,
             graphiql: true,
             enhanceGraphiql: true,
@@ -20,7 +21,6 @@ for (const [service, connectionString] of Object.entries(services)) {
             appendPlugins: [
                 PaginationFilterPlugin,
                 ConnectionFilterPlugin,
-                MutationBlockerPlugin,
             ],
         })
     );
